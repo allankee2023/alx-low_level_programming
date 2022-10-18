85d824b134aae0dc44b422c51c3faf45865b88ba
allankee2023 <chibambaallan1997@gmail.com>
2022-10-18 22:03:08 +0200

    jklee

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,47 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_times_table - Prints a multiplication table up to param
- * @n: The number to be treated
- *
- * Return: Number matrix
- */
+ * print_times_table - prints the n times table, starting with 0.
+ * @n: number of the times table
+**/
+
 void print_times_table(int n)
 {
-	int x, y, z;
+	int i, j, k, l;
 
-	if (n >= 0 && n <= 14)
+	if (n <= 15 && n >= 0)
 	{
-		for (x = 0; x <= n; x++)
+		for (i = 0; i <= n; i++)
 		{
-			for (y = 0; y <= n; y++)
+			_putchar('0');
+
+			for (j = 1; j <= n; j++)
 			{
-				z = x * y;
-				if (z > 99)
+				_putchar(',');
+				_putchar(' ');
+
+				k = i * j;
+
+				if (k <= 9)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar(' ');
+					_putchar(' ');
 				}
-				else if (z > 9)
+				else if (k <= 99 && k >= 10)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
 				}
 				else
 				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
+					l = k / 10;
+					_putchar((l / 10) + '0');
+					_putchar((l % 10) + '0');
 				}
+
+				_putchar((k % 10) + '0');
 			}
+
 			_putchar('\n');
 		}
 	}
